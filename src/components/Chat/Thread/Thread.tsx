62a69442d8f5d84fab50c973be46ad4488e0f3ea
Pournami Raju <pournami.raju@aspiresys.kl>
2026-08-19 22:06:55 +0530

    scroll fix

--- src/components/Chat/Thread/Thread.tsx
+++ src/components/Chat/Thread/Thread.tsx
@@ -343,7 +343,7 @@ const Thread: React.FC<ThreadProps> = ({
         onExpand={onExpand}
       />
       <ThreadPrimitive.Root className="flex min-h-0 flex-1 flex-col">
-        <ThreadPrimitive.Viewport
+        <ThreadPrimitive.Viewport autoScroll={false}
           className={
             mode === 'fullscreen'
               ? 'chat-scrollbar relative flex min-h-0 flex-1 flex-col overflow-y-auto overscroll-contain bg-transparent pt-20'
